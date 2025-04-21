574c2f2fd3719018d61a0da1c4d0622d15cc25e5
Abdalrahman Ahmed Abdalrahman Ali <147574424+Daramli@users.noreply.github.com>
2025-04-21 04:27:31 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ export default {
   ],
   theme: {
     extend: {
+      height: {
+        "screen/2": "50vh",
+        "screen/3": "calc(100vh / 3)",
+        "screen/4": "calc(100vh / 4)",
+      },
       colors: {
         brandRed: '#380000', // لون أحمر غامق
       },
